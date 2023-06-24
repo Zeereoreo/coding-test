37c778dcdc2695d830d28e58d8f6dc1baab4a43a
Zeereoreo <wltjrcjf12@naver.com>
2023-06-24 11:14:00 +0900

    test : 2

--- CT 2.js
+++ CT 2.js
@@ -1,4 +1,6 @@
-// 정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+// 문자열 str과 정수 n이 주어집니다.
+// str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
 
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -11,6 +13,7 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
+    str = input[0];
+    n = Number(input[1]);
+    console.log(`${str}`.repeat(n))
 });
-
